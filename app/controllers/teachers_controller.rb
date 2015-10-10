44a28090a7647884098eb707d7d210fbe4ce5084
Blake Strickland <Stricklandb02@gmail.com>
2015-10-10 14:58:38 -0400

    Add nested Grades in Students in Teachers

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -57,7 +57,9 @@ class TeachersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def teacher_params
-      params.require(:teacher).permit( :name, :email, :password, :student_attributes[:name, :email, :password, :teacher_id])
+      params.require(:teacher).permit( :name, :email, :password,
+      :student_attributes[:name, :email, :password, :teacher_id],
+      :grade_attributes[:student_id, :assignment_name, :grade])
     end
 
     def others_allowed_access?
